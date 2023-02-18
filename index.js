2e8948d55fc4d1f30293c7210ef800a7dd4ab6d1
Aman Gupta <amangupta99344@gmail.com>
2023-02-18 23:09:52 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const hello = <div className="AboutUs">
+    <h1>About Us</h1>
+    <p>At Zappos.com. our purpose is simple: to live and dewer WOW.</p>
+     <p>Tvænty years ago. we began as a small online retailer that only sold shoes. Today. we still sell shoes — as well as
+clothing handbags. accessories, and more. That "more" is providing the very best customer service, customer
+experience. and company culture. We aim to inspire the world by showing it'S possible to simultaneously deliver
+happiness to customers, employees. vendors. shareholders. and the community in a long-term. sustainable way.</p>
+<p>We hope that in the future people won't even realize we started selling shoes online. Instead. they'll know Zappos as a
+service company that just happens to sell_____</p>
+</div>
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(hello)
